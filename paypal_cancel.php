5804f5110f43fff5576c00dddee65a422f123759
rahul gupta <rahulbsl1994@gmail.com>
2019-04-07 23:03:53 +0600

    project completed

---
+++ paypal_cancel.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+?>
+<html>
+	<head>
+		<title>Payment Cancel!</title>
+	</head>
+	
+	<body>
+	
+	<h2>Payment was cancelled!</h2>
+	<h3>Your payment was not successful, please go back our shop and try again. </h3>
+	
+	
+	</body>
+	
+</html>
+		
---
+++ paypal_success.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+?>
+<html>
+	<head>
+		<title>Payment Successful!</title>
+	</head>
+	
+	<body>
+	
+	<h2>Welcome <?php echo $_SESSION['customer_email']; ?></h2>
+	<h3>Your payment was successful, please go to your account </h3>
+	
+	
+	</body>
+	
+</html>
+		
